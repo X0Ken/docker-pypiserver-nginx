88d24af34e05917448cd296a8c8ec363d261383d
wanghongxu <wang19930902@gmail.com>
2017-06-21 14:44:49 +0800

    remove auth

--- pypiserver/docker-entrypoint.sh
+++ pypiserver/docker-entrypoint.sh
@@ -4,7 +4,8 @@ if [ "$1" = 'pypiserver' ]; then
     echo "[RUN]: Launching pypiserver"
     mkdir -p /data/packages
     touch /data/.htaccess
-    exec gunicorn -w16 'pypiserver:app(root="/data/packages", password_file="/data/.htaccess")' -b 0.0.0.0:80
+    
+    exec gunicorn -w16 'pypiserver:app(root="/data/packages", authenticated=[], password_file="/data/.htaccess")' -b 0.0.0.0:80
 fi
 
 echo "[RUN]: Builtin command not provided [pypiserver]"
